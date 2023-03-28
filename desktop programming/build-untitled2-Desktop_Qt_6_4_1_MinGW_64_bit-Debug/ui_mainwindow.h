537b7af3f70fa3eec690004c51b0f2d6245f0612
KwRafik <rafalkowalczyk411@gmail.com>
2023-03-28 20:58:49 +0200

    Pogodex
    Dokończenie ui chyba

--- desktop programming/build-untitled2-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
+++ desktop programming/build-untitled2-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,12 +11,13 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QFormLayout>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
 #include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
@@ -37,10 +38,19 @@ public:
     QPushButton *ButtonPogodexSki;
     QPushButton *ButtonPogodexSafe;
     QPushButton *ButtonPogodexWeek;
-    QWidget *horizontalLayoutWidget;
-    QHBoxLayout *horizontalLayout;
-    QLineEdit *lineEdit;
-    QPushButton *pushButton;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QPushButton *ButtonSearch;
+    QLineEdit *Miasto;
+    QTextBrowser *dane;
+    QTabWidget *cos;
+    QWidget *Pn;
+    QWidget *Wt;
+    QWidget *Sr;
+    QWidget *Czw;
+    QWidget *Pt;
+    QWidget *Sob;
+    QWidget *Nd;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -48,12 +58,14 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(1283, 748);
+        MainWindow->resize(1041, 748);
+        MainWindow->setMinimumSize(QSize(1041, 748));
+        MainWindow->setMaximumSize(QSize(1041, 748));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         verticalLayoutWidget = new QWidget(centralwidget);
         verticalLayoutWidget->setObjectName("verticalLayoutWidget");
-        verticalLayoutWidget->setGeometry(QRect(20, 26, 1241, 81));
+        verticalLayoutWidget->setGeometry(QRect(20, 26, 1001, 81));
         Banner = new QVBoxLayout(verticalLayoutWidget);
         Banner->setObjectName("Banner");
         Banner->setContentsMargins(0, 0, 0, 0);
@@ -111,36 +123,70 @@ public:
 
         Menu->addWidget(ButtonPogodexWeek);
 
-        horizontalLayoutWidget = new QWidget(centralwidget);
-        horizontalLayoutWidget->setObjectName("horizontalLayoutWidget");
-        horizontalLayoutWidget->setGeometry(QRect(170, 120, 261, 31));
-        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
-        horizontalLayout->setObjectName("horizontalLayout");
-        horizontalLayout->setContentsMargins(0, 0, 0, 0);
-        lineEdit = new QLineEdit(horizontalLayoutWidget);
-        lineEdit->setObjectName("lineEdit");
-
-        horizontalLayout->addWidget(lineEdit);
-
-        pushButton = new QPushButton(horizontalLayoutWidget);
-        pushButton->setObjectName("pushButton");
-        pushButton->setStyleSheet(QString::fromUtf8("image: url(:/imgs/Grafiki/lupa.jpg);\n"
+        formLayoutWidget = new QWidget(centralwidget);
+        formLayoutWidget->setObjectName("formLayoutWidget");
+        formLayoutWidget->setGeometry(QRect(780, 120, 241, 571));
+        formLayout = new QFormLayout(formLayoutWidget);
+        formLayout->setObjectName("formLayout");
+        formLayout->setContentsMargins(0, 0, 0, 0);
+        ButtonSearch = new QPushButton(formLayoutWidget);
+        ButtonSearch->setObjectName("ButtonSearch");
+        ButtonSearch->setStyleSheet(QString::fromUtf8("image: url(:/imgs/Grafiki/lupa.jpg);\n"
 "width: 22px;\n"
 "height: 22px;\n"
 "border: 1px solid black;"));
 
-        horizontalLayout->addWidget(pushButton);
+        formLayout->setWidget(0, QFormLayout::LabelRole, ButtonSearch);
 
+        Miasto = new QLineEdit(formLayoutWidget);
+        Miasto->setObjectName("Miasto");
+
+        formLayout->setWidget(0, QFormLayout::FieldRole, Miasto);
+
+        dane = new QTextBrowser(formLayoutWidget);
+        dane->setObjectName("dane");
+
+        formLayout->setWidget(1, QFormLayout::SpanningRole, dane);
+
+        cos = new QTabWidget(centralwidget);
+        cos->setObjectName("cos");
+        cos->setGeometry(QRect(140, 120, 631, 571));
+        cos->setTabShape(QTabWidget::Triangular);
+        Pn = new QWidget();
+        Pn->setObjectName("Pn");
+        Pn->setStyleSheet(QString::fromUtf8(""));
+        cos->addTab(Pn, QString());
+        Wt = new QWidget();
+        Wt->setObjectName("Wt");
+        cos->addTab(Wt, QString());
+        Sr = new QWidget();
+        Sr->setObjectName("Sr");
+        cos->addTab(Sr, QString());
+        Czw = new QWidget();
+        Czw->setObjectName("Czw");
+        cos->addTab(Czw, QString());
+        Pt = new QWidget();
+        Pt->setObjectName("Pt");
+        cos->addTab(Pt, QString());
+        Sob = new QWidget();
+        Sob->setObjectName("Sob");
+        cos->addTab(Sob, QString());
+        Nd = new QWidget();
+        Nd->setObjectName("Nd");
+        cos->addTab(Nd, QString());
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 1283, 26));
+        menubar->setGeometry(QRect(0, 0, 1041, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
+
+        cos->setCurrentIndex(0);
+
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -155,13 +201,35 @@ public:
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:28pt; font-weight:700;\">Pogodex</span></p></body></html>", nullptr));
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:28pt; font-weight:700;\">Pogodex Week</span></p></body></html>", nullptr));
         ButtonPogodexTravel->setText(QString());
         ButtonPogodexSail->setText(QString());
         ButtonPogodexSki->setText(QString());
         ButtonPogodexSafe->setText(QString());
         ButtonPogodexWeek->setText(QString());
-        pushButton->setText(QString());
+        ButtonSearch->setText(QString());
+#if QT_CONFIG(accessibility)
+        Miasto->setAccessibleDescription(QString());
+#endif // QT_CONFIG(accessibility)
+        Miasto->setInputMask(QString());
+        Miasto->setText(QString());
+        Miasto->setPlaceholderText(QCoreApplication::translate("MainWindow", "Wpisz Miasto", nullptr));
+#if QT_CONFIG(tooltip)
+        cos->setToolTip(QCoreApplication::translate("MainWindow", "<html><head/><body><p>Pn</p></body></html>", nullptr));
+#endif // QT_CONFIG(tooltip)
+        cos->setTabText(cos->indexOf(Pn), QCoreApplication::translate("MainWindow", "     Pn     ", nullptr));
+#if QT_CONFIG(whatsthis)
+        Wt->setWhatsThis(QString());
+#endif // QT_CONFIG(whatsthis)
+#if QT_CONFIG(accessibility)
+        Wt->setAccessibleName(QString());
+#endif // QT_CONFIG(accessibility)
+        cos->setTabText(cos->indexOf(Wt), QCoreApplication::translate("MainWindow", "     Wt     ", nullptr));
+        cos->setTabText(cos->indexOf(Sr), QCoreApplication::translate("MainWindow", "     \305\232r     ", nullptr));
+        cos->setTabText(cos->indexOf(Czw), QCoreApplication::translate("MainWindow", "     Czw     ", nullptr));
+        cos->setTabText(cos->indexOf(Pt), QCoreApplication::translate("MainWindow", "     Pt     ", nullptr));
+        cos->setTabText(cos->indexOf(Sob), QCoreApplication::translate("MainWindow", "     Sob     ", nullptr));
+        cos->setTabText(cos->indexOf(Nd), QCoreApplication::translate("MainWindow", "     Nd     ", nullptr));
     } // retranslateUi
 
 };
